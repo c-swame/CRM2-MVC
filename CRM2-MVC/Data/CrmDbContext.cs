b5b5ddb6a5fd626764decf396dd27f15428c0f4a
JoseAlguem <jose.alguem142@gmail.com>
2022-12-14 22:12:42 -0300

    Chore[Data context]: Creates a context to the database

---
+++ CRM2-MVC/Data/CrmDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CRM2_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM2_MVC.Data
+{
+    public class CrmDbContext : DbContext
+    {
+        public CrmDbContext(DbContextOptions<CrmDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<LeadModel> Leads { get; set; } 
+        public DbSet<CourseModel> Courses { get; set; }
+
+    }
+}
